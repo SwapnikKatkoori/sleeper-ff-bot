b480e7c77db846afe9a5d23c4e2f9061c4cb26d5
Swapnik Katkoori <katkoor2@msu.edu>
2019-06-22 19:04:31 -0400

    initial setup

---
+++ sleeper_ff_bot/discord.py
@@ -0,0 +1,7 @@
+
+class Discord():
+	def __init__(self):
+		pass
+
+	def send_message(self, message):
+		pass
---
+++ sleeper_ff_bot/group_me.py
@@ -0,0 +1,7 @@
+
+class GroupMe():
+	def __init__(self):
+		pass
+
+	def send_message(self, message):
+		pass
