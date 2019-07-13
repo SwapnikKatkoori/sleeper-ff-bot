8d2356bf29cad6d3ce6a996629caa97283a72974
Swapnik Katkoori <katkoor2@msu.edu>
2019-07-13 18:39:21 -0400

    added slack integration

--- sleeper_ff_bot/slack.py
+++ sleeper_ff_bot/slack.py
@@ -1,6 +1,10 @@
-class Slack:
-    def __init__(self):
-        pass
+import requests
+from bot_interface import BotInterface
+
+
+class Slack(BotInterface):
+    def __init__(self, webhook):
+        self.webhook = webhook
 
     def send_message(self, message):
-        pass
+        requests.post(self.webhook, json={"text": message})
