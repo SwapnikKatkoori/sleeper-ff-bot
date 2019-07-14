8e30f6a2dbac2b6034ab235b51d8810b150286ac
Swapnik Katkoori <katkoor2@msu.edu>
2019-07-14 00:31:48 -0400

    added scheduling

--- sleeper_ff_bot/bot.py
+++ sleeper_ff_bot/bot.py
@@ -172,7 +172,7 @@ if __name__ == "__main__":
         webhook = os.environ["DISCORD_WEBHOOK"]
         bot = Discord(webhook)
 
-    schedule.every().sunday.at("0:30").do(bot.send, get_close_games, league_id, 30)
+    schedule.every().sunday.at("0:35").do(bot.send, get_close_games, league_id, 30)
     schedule.every().sunday.at("19:00").do(bot.send, get_close_games, league_id, 30)  # Close games on 7:00
     schedule.every().monday.at("8:00").do(bot.send, get_scores, league_id)  # Miracle Monday at 12:00 pm on Monday
     schedule.every().tuesday.at("6:30").do(bot.send, get_standings, league_id)  # Standings at 6:30 am on Tuesday
