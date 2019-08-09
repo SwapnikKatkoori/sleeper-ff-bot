7a8b85023417cea20ae9ec84360b2de924311fed
AlexHawkins1 <alex.hawkins1@gmail.com>
2019-08-09 11:46:38 -0400

    Update bot.py

--- sleeper_ff_bot/bot.py
+++ sleeper_ff_bot/bot.py
@@ -598,7 +598,7 @@ if __name__ == "__main__":
     schedule.every().day.at("22:36").do(bot.send, get_player_name).tag('schedule-1')
 
     # Off-Season
-    schedule.every().day.at("15:27").do(bot.send, get_draft_order).tag('schedule-4')
+    schedule.every().day.at("15:48").do(bot.send, get_draft_order).tag('schedule-4')
 
     while True:
         if starting_date <= pendulum.today():
