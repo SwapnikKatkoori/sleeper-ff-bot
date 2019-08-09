6c4e5a592956972c2df9086679f2cf887983a1b8
AlexHawkins1 <alex.hawkins1@gmail.com>
2019-08-09 11:58:50 -0400

    Update bot.py

--- sleeper_ff_bot/bot.py
+++ sleeper_ff_bot/bot.py
@@ -49,13 +49,12 @@ def get_spoob_predict():
 def get_draft_order():
     logging.error("Running Draft Order")
     # use creds to create a client to interact with the Google Drive API
-    scopes = ['https://spreadsheets.google.com/feeds',
-         'https://www.googleapis.com/auth/drive']
+    scope = ['https://spreadsheets.google.com/feeds' + ' ' +'https://www.googleapis.com/auth/drive']
     json_creds = os.environ["KEY"]
 
     creds_dict = json.loads(json_creds)
     creds_dict["private_key"] = creds_dict["private_key"].replace("\\\\n", "\n")
-    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scopes)
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
     client = gspread.authorize(creds)
 
     sh = client.open_by_url("https://docs.google.com//dspreadsheets/185PXeVLTFaCHt_m3Mw3XeE1w0UGCmzVhZHMKw3UJeHE/edit#gid=1327629526")
@@ -599,7 +598,7 @@ if __name__ == "__main__":
     schedule.every().day.at("22:36").do(bot.send, get_player_name).tag('schedule-1')
 
     # Off-Season
-    schedule.every().day.at("15:56").do(bot.send, get_draft_order).tag('schedule-4')
+    schedule.every().day.at("16:00").do(bot.send, get_draft_order).tag('schedule-4')
 
     while True:
         if starting_date <= pendulum.today():
