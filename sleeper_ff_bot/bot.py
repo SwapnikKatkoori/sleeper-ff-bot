03ba96c66ecbc37f1fe281138fa3ec6dce2dfbba
AlexHawkins1 <alex.hawkins1@gmail.com>
2019-08-14 21:29:57 -0400

    Update bot.py

--- sleeper_ff_bot/bot.py
+++ sleeper_ff_bot/bot.py
@@ -160,7 +160,8 @@ def get_player_key(search_name, requestor):
     for k in players:
         player = json.loads(k)
         logging.error(search_name)
-        logging.error(player["search_full_name"])
+        test = player["search_full_name"]
+        logging.error(test)
         if player["search_full_name"] == search_name:
             found_players.append((player, player['full_name'], player['position'], player['team'], [requestor]))
         if len(found_players) > 1:
