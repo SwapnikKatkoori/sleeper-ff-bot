caa9b0099c8efcff4d0cf4d222a2f7d44a92e8b0
AlexHawkins1 <alex.hawkins1@gmail.com>
2019-08-16 10:04:10 -0400

    Update bot.py

--- sleeper_ff_bot/bot.py
+++ sleeper_ff_bot/bot.py
@@ -180,19 +180,16 @@ def get_player_key(search_string, requestor, name_key_switch):
                     found_players.append((player_id, player["full_name"], player["position"], player["team"], [requestor]))
             except:
                 pass
-            try:
-                if player["position"] == "DEF":
-                    logging.error(player["position"])
-                    search_name =  player["first_name"].lower() + player["last_name"].lower()
-                    search_name = search_name.replace(" ","")
-                    def_token_set_ratio = fuzz.ratio(search_string, search_name)
-                    logging.error(search_string)
-                    logging.error(search_name)
-                    logging.error(def_token_set_ratio)
-                    if def_token_set_ratio > 54:
-                        found_players.append((player_id, player["full_name"], player["position"], player["team"], [requestor]))
-            except:
-                pass
+            if player["position"] == "DEF":
+                logging.error(player["position"])
+                search_name =  player["first_name"].lower() + player["last_name"].lower()
+                search_name = search_name.replace(" ","")
+                def_ratio = fuzz.ratio(search_string, search_name)
+                logging.error(search_string)
+                logging.error(search_name)
+                logging.error(def_ratio)
+                if def_ratio > 54:
+                    found_players.append((player_id, player["full_name"], player["position"], player["team"], [requestor]))
         if len(found_players) > 1:
             text = "Which player are you looking for?\n\n"
             for p in found_players:
