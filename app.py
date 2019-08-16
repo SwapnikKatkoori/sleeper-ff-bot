d072ee0030ceba1f6d4592f4b16aac968ef93908
AlexHawkins1 <alex.hawkins1@gmail.com>
2019-08-16 10:35:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -35,47 +35,48 @@ def webhook():
     logging.error(os.environ["WAITING_FOR_RESPONSE"])
     # 'message' is an object that represents a single GroupMe message.
     message = request.get_json()
-    time.sleep(3)
-    if os.environ["WAITING_FOR_RESPONSE"] == "True" and not sender_is_bot(message):
-        try:
-            int(message['text'].lower())
-            stat_response = True
-        except:
-            stat_response = False
-        if stat_response == True and not sender_is_bot(message):
-            get_player_key(message['text'],message['name'].lower(),1)
-            os.environ["WAITING_FOR_RESPONSE"] = "False"
-        elif 'none' in message['text'].lower() and not sender_is_bot(message)::
-            bot.send(send_any_string,"Sorry I failed you.")
-            os.environ["WAITING_FOR_RESPONSE"] = "False"
-    if '@bot' in message['text'].lower()  and not sender_is_bot(message):
-        if 'adam' in message['name'].lower():
-            bot.send(send_any_string,'Fuck off Adam')
-        elif 'draft order' in message['text'].lower():
-            bot.send(get_draft_order)
-        elif 'who' in message['text'].lower():
-            bot.send(get_player_name)
-        elif 'fun fact' in message['text'].lower():
-            bot.send(get_fun_fact)
-        elif 'rule changes' in message['text'].lower():
-            bot.send(get_rule_changes)
-        elif 'standings' in message['text'].lower():
-            bot.send(get_standings_string, league_id)
-        elif 'score update' in message['text'].lower():
-            bot.send(get_scores_string, league_id)
-        elif 'matchups' in message['text'].lower():
-            bot.send(get_matchups_string, league_id)
-        elif 'stats' in message['text'].lower():
-            text = message['text']
-            text = text.replace("@bot","")
-            text = text.replace("stats", "")
-            text = text.replace(" ","")
-            text = text.lower()
-            waiting = get_player_key(text, message['name'].lower(),0)
-            if waiting == True:
-                os.environ["WAITING_FOR_RESPONSE"] = "True"
-        else:
-            bot.send(send_any_string, 'I am unsure.')
+    if not sender_is_bot(message):
+        time.sleep(3)
+        if os.environ["WAITING_FOR_RESPONSE"] == "True":
+            try:
+                int(message['text'].lower())
+                stat_response = True
+            except:
+                stat_response = False
+            if stat_response == True:
+                get_player_key(message['text'],message['name'].lower(),1)
+                os.environ["WAITING_FOR_RESPONSE"] = "False"
+            elif 'none' in message['text'].lower():
+                bot.send(send_any_string,"Sorry I failed you.")
+                os.environ["WAITING_FOR_RESPONSE"] = "False"
+        if '@bot' in message['text'].lower():
+            if 'adam' in message['name'].lower():
+                bot.send(send_any_string,'Fuck off Adam')
+            elif 'draft order' in message['text'].lower():
+                bot.send(get_draft_order)
+            elif 'who' in message['text'].lower():
+                bot.send(get_player_name)
+            elif 'fun fact' in message['text'].lower():
+                bot.send(get_fun_fact)
+            elif 'rule changes' in message['text'].lower():
+                bot.send(get_rule_changes)
+            elif 'standings' in message['text'].lower():
+                bot.send(get_standings_string, league_id)
+            elif 'score update' in message['text'].lower():
+                bot.send(get_scores_string, league_id)
+            elif 'matchups' in message['text'].lower():
+                bot.send(get_matchups_string, league_id)
+            elif 'stats' in message['text'].lower():
+                text = message['text']
+                text = text.replace("@bot","")
+                text = text.replace("stats", "")
+                text = text.replace(" ","")
+                text = text.lower()
+                waiting = get_player_key(text, message['name'].lower(),0)
+                if waiting == True:
+                    os.environ["WAITING_FOR_RESPONSE"] = "True"
+            else:
+                bot.send(send_any_string, 'I am unsure.')
 
     return "ok",200
 
