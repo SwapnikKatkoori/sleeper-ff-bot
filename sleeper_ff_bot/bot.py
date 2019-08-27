ba406124467248237e61141e2fbc1e8a4c6c9705
AlexHawkins1 <alex.hawkins1@gmail.com>
2019-08-27 10:41:03 -0400

    Update bot.py

--- sleeper_ff_bot/bot.py
+++ sleeper_ff_bot/bot.py
@@ -189,6 +189,9 @@ def get_depth_chart(team, position):
     dc_num = []
     for player_id in players:
         player = players[player_id]
+        logging.error(player["team"])
+        logging.error(player["position"])
+        logging.error(player["depth_chart_order"])
         if player["team"] == team and player["position"] == position:
             dc_num.append(player["depth_chart_order"])
             logging.error(player["depth_chart_order"])
