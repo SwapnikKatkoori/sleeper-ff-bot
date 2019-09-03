3b1b0b1387ae82df9539a6b67ba7a04265e63a18
AlexHawkins1 <alex.hawkins1@gmail.com>
2019-09-03 11:55:33 -0400

    Update bot.py

--- sleeper_ff_bot/bot.py
+++ sleeper_ff_bot/bot.py
@@ -374,7 +374,6 @@ def get_player_stats(search_object):
         year = STARTING_YEAR
     else:
         year = int(STARTING_YEAR) - 1
-    logging.error(year)
     stats = Stats(). get_all_stats("regular",year)
 
     bot_type = os.environ["BOT_TYPE"]
@@ -407,7 +406,7 @@ def get_player_stats(search_object):
         stats_run = False
     if stats_run:
         if position is not "DEF":
-            final_string = "{} ({} - {})({})\n{}\n\n".format(player_name, position, team, player, injury_status)
+            final_string = "{} ({} - {})\n{}\n\n".format(player_name, position, team, injury_status)
         else:
             final_string = "{} ({} - {})\n\n".format(player_name, position, team)
 
